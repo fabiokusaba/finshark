cf3441d89b0d4f8def263f3cc4b33b7f2fe9444d
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-08 08:39:17 -0300

    adicionado enpoint create

--- api/Mappers/StockMappers.cs
+++ api/Mappers/StockMappers.cs
@@ -22,5 +22,18 @@ namespace api.Mappers
                 MarketCap = stockModel.MarketCap
             };
         }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purschase = stockDto.Purschase,
+                LastDividend = stockDto.LastDividend,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
+            };
+        }
     }
 }
