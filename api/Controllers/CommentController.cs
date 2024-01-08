3b97426c1f033a7acdd58f8091b512da5ab53fcf
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-08 10:06:40 -0300

    adicionado Comment

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/comment")]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GeAllAsync();
+
+            var commentDto = comments.Select(c => c.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+    }
+}
