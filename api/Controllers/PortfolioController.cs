ea737d66ff4e56e8cd73e5625e00c828fa80796b
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-27 20:37:39 -0300

    Add endpoint Get User Portfolio

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -1,5 +1,7 @@
-﻿using api.Interfaces;
+﻿using api.Extensions;
+using api.Interfaces;
 using api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +13,25 @@ public class PortfolioController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly IStockRepository _stockRepo;
+    private readonly IPortfolioRepository _portfolioRepo;
     
-    public PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepo)
+    public PortfolioController(
+        UserManager<AppUser> userManager, 
+        IStockRepository stockRepo, 
+        IPortfolioRepository portfolioRepo)
     {
         _userManager = userManager;
         _stockRepo = stockRepo;
+        _portfolioRepo = portfolioRepo;
     }
-    
-    
+
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> GetUserPortfolio()
+    {
+        var username = User.GetUsername();
+        var appUser = await _userManager.FindByNameAsync(username);
+        var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+        return Ok(userPortfolio);
+    }
 }
