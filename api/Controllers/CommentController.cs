7ea15545958538b82c82de4ff091a9487d5b558e
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-27 21:34:43 -0300

    Added User in create comment

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Comment;
+using api.Extensions;
 using api.Interfaces;
 using api.Mappers;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -17,10 +20,16 @@ namespace api.Controllers
 
         private readonly IStockRepository _stockRepo;
 
-        public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
+        private readonly UserManager<AppUser> _userManager;
+
+        public CommentController(
+            ICommentRepository commentRepo, 
+            IStockRepository stockRepo, 
+            UserManager<AppUser> userManager)
         {
             _commentRepo = commentRepo;
             _stockRepo = stockRepo;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -69,7 +78,11 @@ namespace api.Controllers
                 return BadRequest("Stock does not exist");
             }
 
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+
             var commentModel = commentDto.ToCommentFromCreate(stockId);
+            commentModel.AppUserId = appUser.Id;
 
             await _commentRepo.CreateAsync(commentModel);
 
